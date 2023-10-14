59e2996faec10a14f5e7dab1fd6970ea2e2b1472
DESKTOP-F01M0EB\j.onischtschenko <rzhekislaw@gmail.com>
2023-10-15 00:43:58 +0300

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace ns
+{
+    class tClass
+    {
+        private static Semaphore semaphore = new Semaphore(1, 1);
+
+        public static void Main() 
+        {
+            Console.WriteLine("Semaphore(1, 1);");
+            Console.WriteLine("Start time: " + DateTime.Now.ToString());
+            for (int i = 1; i <= 10; i++)
+            {
+                var thread = new Thread(async () => { await ReadAsync(); });
+                thread.Start();
+            }
+        }
+
+        public async static Task ReadAsync()
+        {
+            semaphore.WaitOne();
+
+            using (var reader = new StreamReader("file.txt"))
+            {
+                var tmp = reader.ReadToEnd();
+                Console.WriteLine(DateTime.Now.ToString());
+            }
+
+            semaphore.Release();
+        }
+    }
+}
